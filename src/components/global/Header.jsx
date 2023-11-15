5dec6ed1c0d78aed1e0ce3559c3b34d544e87fab
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-11-15 11:46:49 +0600

    mobile menu updated

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -244,6 +244,21 @@ const Header = () => {
               {currentWidth < 990 && menuOpen && (
                 <div className="menu-container-mobile">
                   <div className="nav-menu-container-mobile">
+                    {lngMode == "ar" && (
+                      <div
+                        onClick={() => setMenuOpen(!menuOpen)}
+                        style={{ paddingBottom: "10px" }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="2em"
+                          viewBox="0 0 384 512"
+                          fill="#fff"
+                        >
+                          <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                        </svg>
+                      </div>
+                    )}
                     <div
                       className="nav-menu-item-mobile"
                       onClick={() => setMenuOpen(false)}
@@ -321,8 +336,8 @@ const Header = () => {
                           }
                         >
                           {lngMode == "en"
-                          ? nav?.login_register?.en
-                          : nav?.login_register?.ar}
+                            ? nav?.login_register?.en
+                            : nav?.login_register?.ar}
                         </NavLink>
                       )}
 
@@ -334,7 +349,9 @@ const Header = () => {
                           to="/"
                           className="nir-btn white book-now-btn mt-3"
                         >
-                           {lngMode == "en" ? nav?.book_now?.en : nav?.book_now?.ar}
+                          {lngMode == "en"
+                            ? nav?.book_now?.en
+                            : nav?.book_now?.ar}
                         </Link>
                       </div>
                     </div>
